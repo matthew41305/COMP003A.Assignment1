35014806806f0a1b47bbd954f92593231c783768
matth <matth@DESKTOP-3D0678T>
2024-01-22 00:30:47 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,114 @@
+﻿// Author:      Matthew Hudson
+// Class:       COMP-003A-LO1
+// Purpose:     Code for assignment 1
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things about me");
+            Console.WriteLine("1. I have a dog");
+            Console.WriteLine("2. My dog's name is Muffin");
+            Console.WriteLine("3. I like to go to the gym");
+            Console.WriteLine("3. My favorite food is wings");
+            Console.WriteLine("4. I'm lactose intolerant");
+            Console.WriteLine("5. I love icecream");
+            Console.WriteLine("6. I have a yellow car");
+            Console.WriteLine("7. I'm a computer scienc major");
+            Console.WriteLine("8. I play a lot of fps games");
+            Console.WriteLine("9. Roblox used to be my favorite game as a kid");
+            Console.WriteLine("10. I'm Filipino and American");
+            Console.WriteLine("11. I have four cats");
+            Console.WriteLine("12. I've been to the Philippines 3 times");
+            Console.WriteLine("13. Most of my family is from the Philippines");
+            Console.WriteLine("14. Cyber security is a career path I'm interested in");
+            Console.WriteLine("15. I have a gaming keyboard with yellow switches");
+            Console.WriteLine("16. I used to stream on twitch a lot");
+            Console.WriteLine("17. I like to collect pop figures from overwatch");
+            Console.WriteLine("18. I play comptetive overwatch with my cousin and we're pro");
+            Console.WriteLine("19. Some offline games I like are chess and chinese checkers");
+            Console.WriteLine("20. I have a lot of posters from different movies");
+            Console.WriteLine("21. I plan on traveling the world one day");
+            Console.WriteLine("22. I've found this career very interesting");
+            Console.WriteLine("23. The first thing I've ever coded was in Roblox");
+            Console.WriteLine("24. I used to like making Roblox games");
+            Console.WriteLine("25. My first game was a sword fighting game");
+            Console.WriteLine("26. When I was young I wanted to be a youtuber");
+            Console.WriteLine("27. The first youtuber I met was GuavaJuice");
+            Console.WriteLine("28. My favorite color is black or blue");
+            Console.WriteLine("29. My favorite soda is coca-cola, but it's turning into dr.pepper");
+            Console.WriteLine("30. My favorite superhero is spiderman");
+            Console.WriteLine("31. I want to visit Japan one day");
+            Console.WriteLine("32. I have a little brother named Samuel");
+            Console.WriteLine("33. When I was little I wanted to be a hacker");
+            Console.WriteLine("34. I play McCree in overwatch");
+            Console.WriteLine("35. I can do a frontflip");
+            Console.WriteLine("36. I'm very close with my cousin Abby");
+            Console.WriteLine("37. Abby was my roblox friend since day 1");
+            Console.WriteLine("38. My first laptop was an HP");
+            Console.WriteLine("39. My first mouse was a Deathadder from Razer");
+            Console.WriteLine("40. I like to thrift");
+            Console.WriteLine("41. I want to snowboard");
+            Console.WriteLine("42. I have no heater right now");
+            Console.WriteLine("43. I take only cold showers");
+            Console.WriteLine("44. I have a 240hz monitor");
+            Console.WriteLine("45. I build my gaming computer");
+            Console.WriteLine("46. I used to sell snacks at school.");
+            Console.WriteLine("47. My favorite chips takis");
+            Console.WriteLine("48. My favorite icecream is cookies and cream");
+            Console.WriteLine("49. I want to live in Oregon one day");
+            Console.WriteLine("50. I have a friend that lives in NYC");
+            Console.WriteLine("51. I used to own a popular discord server");
+            Console.WriteLine("52. I dyed my hair in quarantine");
+            Console.WriteLine("53. My eyes are dry right now");
+            Console.WriteLine("54. Today all I ate was pizza");
+            Console.WriteLine("55. My favorite resturaunt is wingstop");
+            Console.WriteLine("56. I used to go to my cousins house everyday");
+            Console.WriteLine("57. My mom cooks for a living");
+            Console.WriteLine("58. My mom manages the race track concessions stand");
+            Console.WriteLine("59. I work at the stand");
+            Console.WriteLine("60. For Christmas I'm getting my cousin headless a roblox item");
+            Console.WriteLine("61. I play other games like apex and fornite");
+            Console.WriteLine("62. I thought I would go pro in fortnite");
+            Console.WriteLine("63. My first console was a ps3");
+            Console.WriteLine("64. My favorite console games were Destiny and minecraft");
+            Console.WriteLine("65. I used to sing Justin Bieber as a kid");
+            Console.WriteLine("66. I've alwayds thought garage bands were cool");
+            Console.WriteLine("67. I like a lot of different music");
+            Console.WriteLine("68. I've been listening to a lot of Jazz for these past years");
+            Console.WriteLine("69. My favorite jazz artists is Chet Baker");
+            Console.WriteLine("70. My favorite thing to workout is chest");
+            Console.WriteLine("71. My leg is currently injured");
+            Console.WriteLine("72. I picked up running as a hobby again");
+            Console.WriteLine("73. My favorite sport was basketball");
+            Console.WriteLine("74. The only sport I've played at school was basketball");
+            Console.WriteLine("75. I didn't have many friends as a kid");
+            Console.WriteLine("76. I used to collect nerf guns");
+            Console.WriteLine("77. I work at my church as a sound tech");
+            Console.WriteLine("78. I'm looking for a job as an I.T right now");
+            Console.WriteLine("79. I used to work in the kitchen");
+            Console.WriteLine("80. My first car was a Lexus");
+            Console.WriteLine("81. My new car is mustang");
+            Console.WriteLine("82. This is my first computer class");
+            Console.WriteLine("83. My little brother is 8");
+            Console.WriteLine("84. I have a fast typing speed");
+            Console.WriteLine("85. I used to play games like krunk.r in class");
+            Console.WriteLine("86. I used to use HTML editor to prank my friends");
+            Console.WriteLine("87. I love animals");
+            Console.WriteLine("88. I love learning about animnals");
+            Console.WriteLine("89. I plan on trasnfering to Fresno State");
+            Console.WriteLine("90. I've read the percy jackson series");
+            Console.WriteLine("91. My car keey fob is broken");
+            Console.WriteLine("92. The batter in my fob is dead");
+            Console.WriteLine("93. I need a different keyboard for this class");
+            Console.WriteLine("94. My keyboard has no arrow keys");
+            Console.WriteLine("95. Animal behavior is really intersting");
+            Console.WriteLine("96. I first wanted to be a Doctor");
+            Console.WriteLine("97. Math used to be my favorite subject");
+            Console.WriteLine("98. I like to undertand why things work");
+            Console.WriteLine("99. I understand things better when I know why things work");
+            Console.WriteLine("100. I like the teachers coding account username haha");
+        }
+    }
+}
